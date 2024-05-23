a26591aea1b73f8a1fe224e544112fc1aebef0fc
Heunsig <heun3344@gmail.com>
2024-05-24 00:04:48 +0900

    prototyping polling with WebSocket.

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import express from "express";
+import { WebSocketServer } from "ws";
 import { data } from "./data.js";
 
 const app = express();
 app.use(express.static("public"));
+
 
 const dataArr = data.split("");
 let currentIndex = 0;
@@ -21,6 +23,39 @@ app.get("/data", (_, res) => {
   res.json({ message: word });
 });
 
-app.listen(3000, () => {
+const httpServer = app.listen(3000, () => {
   console.log("Listening on port 3000");
 });
+
+
+const wss = new WebSocketServer({ server: httpServer });
+
+let interval2 = null
+wss.on("connection", (ws) => {
+  ws.on("close", () => {
+    console.log('closed')
+  })
+
+  ws.on("message", (rawData) => {
+    if (currentIndex >= dataArr.length) {
+      currentIndex = 0;
+    }
+
+    const message = decode(rawData)
+    if(message === "start") {
+      interval2 = setInterval(() => {
+        ws.send(getNextWord())
+      }, 100)
+    }
+
+    if(message === "stop") {
+      clearInterval(interval2)
+    }
+  })
+});
+
+
+// function to convert websocket raw data to string
+function decode(data) {
+  return new TextDecoder("utf-8").decode(data);
+}
